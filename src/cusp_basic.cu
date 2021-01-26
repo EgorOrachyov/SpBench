ad1affdfc4150c074393b33a5d5a69142dee4763
Egor19035 <egororachyov5@gmail.com>
2021-01-26 15:01:17 +0300

    [Project] Add and setup basic cusp example

---
+++ src/cusp_basic.cu
@@ -0,0 +1,59 @@
+//
+// Created by Egor.Orachev on 26.01.2021.
+//
+
+// The original example is https://cusplibrary.github.io/md_quickstart.html
+
+#include <cuda.h>
+#include <cusp_fix.hpp>
+
+#include <cusp/hyb_matrix.h>
+#include <cusp/monitor.h>
+#include <cusp/gallery/poisson.h>
+#include <cusp/krylov/cg.h>
+
+// where to perform the computation
+typedef cusp::device_memory MemorySpace;
+
+// which floating point type to use
+typedef float ValueType;
+
+int main()
+{
+    // Version query
+    int cuda_major =  CUDA_VERSION / 1000;
+    int cuda_minor = (CUDA_VERSION % 1000) / 10;
+    int thrust_major = THRUST_MAJOR_VERSION;
+    int thrust_minor = THRUST_MINOR_VERSION;
+    int cusp_major = CUSP_MAJOR_VERSION;
+    int cusp_minor = CUSP_MINOR_VERSION;
+
+    std::cout << "CUDA   v" << cuda_major   << "." << cuda_minor   << std::endl;
+    std::cout << "Thrust v" << thrust_major << "." << thrust_minor << std::endl;
+    std::cout << "Cusp   v" << cusp_major   << "." << cusp_minor   << std::endl;
+
+    // create an empty sparse matrix structure (HYB format)
+    cusp::hyb_matrix<int, ValueType, MemorySpace> A;
+
+    // create a 2d Poisson problem on a 10x10 mesh
+    cusp::gallery::poisson5pt(A, 10, 10);
+
+    // allocate storage for solution (x) and right hand side (b)
+    cusp::array1d<ValueType, MemorySpace> x(A.num_rows, 0);
+    cusp::array1d<ValueType, MemorySpace> b(A.num_rows, 1);
+
+    // set stopping criteria:
+    //  iteration_limit    = 100
+    //  relative_tolerance = 1e-3
+    //  absolute_tolerance = 0
+    //  verbose            = true
+    cusp::monitor<ValueType> monitor(b, 100, 1e-3, 0, true);
+
+    // set preconditioner (identity)
+    cusp::identity_operator<ValueType, MemorySpace> M(A.num_rows, A.num_rows);
+
+    // solve the linear system A * x = b with the Conjugate Gradient method
+    cusp::krylov::cg(A, x, b, monitor, M);
+
+    return 0;
+}
---
+++ src/cusp_fix.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Egor.Orachev on 26.01.2021.
+//
+
+#ifndef SPBENCH_CUSP_FIX_HPP
+#define SPBENCH_CUSP_FIX_HPP
+
+#define __thrust_compiler_fence() do { } while (0);
+
+#endif //SPBENCH_CUSP_FIX_HPP
